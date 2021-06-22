b88e0faaa6e4c4f74f9bd852c5421c6cccdbedc3
brazilec22 <stratienko.artem@gmail.com>
2021-06-23 01:09:51 +0300

    add server

---
+++ MVP/client/ScannerPrice/ScannerPrice/TaskEmployeController/ProductCategory.swift
@@ -0,0 +1,8 @@
+//
+//  ProductCategory.swift
+//  ScannerPrice
+//
+//  Created by Артем Стратиенко on 22.06.2021.
+//
+
+import Foundation
---
+++ MVP/client/ScannerPrice/ScannerPrice/TaskEmployeController/ProductClassCell.swift
@@ -0,0 +1,8 @@
+//
+//  ProductClassCell.swift
+//  ScannerPrice
+//
+//  Created by Артем Стратиенко on 22.06.2021.
+//
+
+import Foundation
